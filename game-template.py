4ca5d9e2757b26bc7f5088d586a2543ab0029798
joncoop <joncoop1972@gmail.com>
2017-05-29 17:41:21 -0400

    Put hero in game (doesn't move yet)

--- game-template.py
+++ game-template.py
@@ -36,7 +36,7 @@ screen = pygame.display.set_mode([SCREEN_WIDTH, SCREEN_HEIGHT])
 pygame.display.set_caption(TITLE)
 clock = pygame.time.Clock()
 
-# Some helper classes
+# Some utility classes
 class TextUtil():
     def display_message(surface, primary_text, secondary_text=None):
         w = surface.get_width()
@@ -55,10 +55,10 @@ class TextUtil():
 
 class ImageUtil():
     def load_image(file_path):
-        return pygame.image.load(file_path).convert()
+        return pygame.image.load(file_path).convert_alpha()
 
     def load_scaled_image(file_path, width=GRID_SIZE, height=GRID_SIZE):
-        img = pygame.image.load(file_path).convert()
+        img = pygame.image.load(file_path).convert_alpha()
         return pygame.transform.scale(img, (width, height))
 
     def reverse_image(img):
@@ -81,21 +81,30 @@ class SoundUtil():
             pygame.mixer.music.play(-1)
 
 # Game assets
-block_types = {"TL": ImageUtil.load_scaled_image("assets/tiles/top_left.png"),
-               "TM": ImageUtil.load_scaled_image("assets/tiles/top_middle.png"),
-               "TR": ImageUtil.load_scaled_image("assets/tiles/top_right.png"),
-               "ER": ImageUtil.load_scaled_image("assets/tiles/end_right.png"),
-               "EL": ImageUtil.load_scaled_image("assets/tiles/end_left.png"),
-               "TP": ImageUtil.load_scaled_image("assets/tiles/top.png"),
-               "CN": ImageUtil.load_scaled_image("assets/tiles/center.png"),
-               "LF": ImageUtil.load_scaled_image("assets/tiles/lone_float.png"),
-               "SP": ImageUtil.load_scaled_image("assets/tiles/special.png")}
+block_images = {"TL": ImageUtil.load_scaled_image("assets/tiles/top_left.png"),
+                "TM": ImageUtil.load_scaled_image("assets/tiles/top_middle.png"),
+                "TR": ImageUtil.load_scaled_image("assets/tiles/top_right.png"),
+                "ER": ImageUtil.load_scaled_image("assets/tiles/end_right.png"),
+                "EL": ImageUtil.load_scaled_image("assets/tiles/end_left.png"),
+                "TP": ImageUtil.load_scaled_image("assets/tiles/top.png"),
+                "CN": ImageUtil.load_scaled_image("assets/tiles/center.png"),
+                "LF": ImageUtil.load_scaled_image("assets/tiles/lone_float.png"),
+                "SP": ImageUtil.load_scaled_image("assets/tiles/special.png")}
+
+item_images = {"Coin": ImageUtil.load_scaled_image("assets/items/coin.png"),
+               "Heart": ImageUtil.load_scaled_image("assets/items/bandaid.png"),
+               "OneUp": ImageUtil.load_scaled_image("assets/items/first_aid.png")}
+
+hero_images = {"Walk": [ImageUtil.load_scaled_image("assets/character/adventurer_walk1.png"),
+                        ImageUtil.load_scaled_image("assets/character/adventurer_walk2.png")],
+               "Jump": [ImageUtil.load_scaled_image("assets/character/adventurer_jump.png")],
+               "Idle": [ImageUtil.load_scaled_image("assets/character/adventurer_idle.png")]}
 
 # Game entities
 class Entity(pygame.sprite.Sprite):
-    def __init__(self, x, y, image):
+    def __init__(self, image, x=0, y=0):
         super().__init__()
-        
+
         self.image = image
         self.rect = self.image.get_rect()
         self.rect.x = x * GRID_SIZE
@@ -112,39 +121,43 @@ class Entity(pygame.sprite.Sprite):
         return abs(self.rect.centerx - other.rect.centerx) < distance
 
     def apply_gravity(self, level):
-        self.vy += self.level.gravity
-        self.vy = min(self.level.gravity, self.level.terminal_velocity)
+        self.vy += level.gravity
+        self.vy = min(level.gravity, level.terminal_velocity)
 
 class Block(Entity):
-    def __init__(self, x, y, kind):
-        super().__init__(x, y, block_types[kind])
+    def __init__(self, image, x, y):
+        super().__init__(image, x, y)
 
 class Hero(Entity):
-    def __init__(self):
-        pass
-
+    def __init__(self, all_images):
+        super().__init__(all_images['Idle'][0])
+        
     def check_boundaries(self, level):
         pass
 
     def move_and_process_blocks(self, blocks):
         pass
 
-    def process_items(self):
+    def process_items(self, items):
         pass
 
-    def process_enemies(self):
+    def process_enemies(self, enemies):
         pass
 
     def update(self, level):
-        level.physics.apply_gravity(self)
-        check_boundaries(level)
-        move_and_process_blocks(level.blocks)
-        process_items(level.items)
-        process_enemies(level.enemies)
+        self.apply_gravity(level)
+        self.check_boundaries(level)
+        self.move_and_process_blocks(level.blocks)
+        self.process_items(level.items)
+        self.process_enemies(level.enemies)
 
+    def reset(self, level):
+        self.rect.x = level.start_x * GRID_SIZE
+        self.rect.y = level.start_y * GRID_SIZE
+        
 class Enemy(Entity):
-    def __init__(self, animation_frames):
-        pass
+    def __init__(self, all_images, x, y):
+        super().__init__(all_images['Idle'][0], x, y)
 
     def reverse(self):
         pass
@@ -155,7 +168,7 @@ class Enemy(Entity):
     def move_and_process_blocks(self, blocks):
         pass
 
-    def update(self, level, hero):
+    def update(self, level):
         pass
 
 class Bear(Enemy):
@@ -167,25 +180,33 @@ class Monster(Enemy):
         pass
 
 class Item(Entity):
-    def __init__(self):
-        pass
+    def __init__(self, image, x, y):
+        super().__init__(image, x, y)
 
     def apply(self, character):
         raise NotImplementedError
 
 class Coin(Item):
-    def __init__(self):
+    def __init__(self, image, x, y):
+        super().__init__(image, x, y)
         self.value = 1
 
     def apply(self, character):
         character.score += self.value
 
 class Heart(Item):
-    def __init__(self):
-        pass
+    def __init__(self, image, x, y):
+        super().__init__(image, x, y)
 
     def apply(self, character):
         character.hearts += 1
+
+class OneUp(Item):
+    def __init__(self, image, x, y):
+        super().__init__(image, x, y)
+
+    def apply(self, character):
+        character.lives += 1
 
 # Scenes
 class Scene():
@@ -200,7 +221,7 @@ class Scene():
 
     def render(self, surface):
         raise NotImplementedError
-
+        
     def change_to_scene(self, next_scene):
         self.next_scene = next_scene
 
@@ -210,12 +231,13 @@ class Scene():
 class TitleScene(Scene):
     def __init__(self):
         super().__init__()
+        self.hero = Hero(hero_images) # Initialize a hero before starting Level scenes
 
     def process_input(self, events, pressed_keys):
         for event in events:
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_SPACE:
-                    self.change_to_scene( Level(0) )
+                    self.change_to_scene( Level(self.hero, 0) )
 
     def update(self):
         '''
@@ -228,24 +250,29 @@ class TitleScene(Scene):
         TextUtil.display_message(screen, "Name of Game", "Press SPACE to start.")
 
 class Level(Scene):
-    def __init__(self, level_num):
+    def __init__(self, hero, level_num):
         super().__init__()
 
+        self.hero = hero
         self.level_num = level_num
         self.completed = False
         self.paused = False
 
         self.blocks = pygame.sprite.Group()
+        self.items = pygame.sprite.Group()
+        self.enemies = pygame.sprite.Group()
+        self.flag = pygame.sprite.Group()
 
         self.active_sprites = pygame.sprite.Group()
         self.inactive_sprites = pygame.sprite.Group()
 
-        self.load(self.level_num)
+        self.load()
 
-    def load(self, level_num):
-        data_file = levels[level_num]
+    def load(self):
+        data_file = levels[self.level_num]
 
         self.starting_blocks = []
+        self.starting_items = []
 
         with open(data_file, 'r') as f:
             data = f.read()
@@ -255,23 +282,42 @@ class Level(Scene):
         self.width = map_data['width'] * GRID_SIZE
         self.height = map_data['height'] * GRID_SIZE
 
-        self.start_x = map_data['start'][0] * GRID_SIZE
-        self.start_y = map_data['start'][1] * GRID_SIZE
-
+        self.gravity = map_data['gravity']
+        self.terminal_velocity = map_data['terminal-velocity']
+
         self.inactive_layer = pygame.Surface([self.width, self.height], pygame.SRCALPHA, 32)
         self.active_layer = pygame.Surface([self.width, self.height], pygame.SRCALPHA, 32)
 
+        self.start_x = map_data['start'][0]
+        self.start_y = map_data['start'][1]
+        
         for item in map_data['blocks']:
             x, y, kind = item[0], item[1], item[2]
-            self.starting_blocks.append( Block(x, y, kind) )
+            img = block_images[kind]
+            self.starting_blocks.append( Block(img, x, y) )
 
+        for item in map_data['items']:
+            x, y, kind = item[0], item[1], item[2]
+            img = item_images[kind]
+
+            if kind == "Coin":
+                self.starting_items.append( Coin(img, x, y) )
+            elif kind == "Heart":
+                self.starting_items.append( Heart(img, x, y) )
+            elif kind == "OneUp":
+                self.starting_items.append( OneUp(img, x, y) )
+
+        # create and draw inactive layer here since we don't need to redraw on each iteration of game loop
         self.inactive_sprites.add(self.starting_blocks)
         self.inactive_sprites.draw(self.inactive_layer)
 
-        #self.active_sprites.add(None)
+        self.setup()
 
-    def reset(self):
-        pass
+    def setup(self):
+        self.hero.reset(self)
+        self.items.add(self.starting_items)
+        
+        self.active_sprites.add(self.hero, self.items)
 
     def process_input(self, events, pressed_keys):
         for event in events:
@@ -282,9 +328,9 @@ class Level(Scene):
                         self.level_num += 1
 
                         if self.level_num < len(levels):
-                            self.change_to_scene( Level(self.level_num) )
+                            self.change_to_scene( Level(self.hero, self.level_num) )
                         else:
-                            self.change_to_scene( Victory() )
+                            self.change_to_scene( Victory(self.hero) )
 
                 else:
                     if event.key == pygame.K_p:
@@ -305,15 +351,15 @@ class Level(Scene):
 
     def update(self):
         if not (self.completed or self.paused):
-            # game is active, update all entities
-            pass
+            self.active_sprites.update(self)
 
     def render(self, surface):
         surface.fill(BLACK)
         if not (self.completed or self.paused):
+            self.active_sprites.draw(self.active_layer)
+
             surface.blit(self.inactive_layer, [0, 0])
-            
-            TextUtil.display_message(surface, str(self.level_num))
+            surface.blit(self.active_layer, [0, 0])
 
         # special messages
         if self.completed:
@@ -322,11 +368,15 @@ class Level(Scene):
             pass
 
 class GameOver(Scene):
-    def __init__(self):
-        super().__init__()
+    def __init__(self, hero):
+        super().__init__(hero)
+        self.hero = hero
 
     def process_input(self, events, pressed_keys):
-        pass
+        for event in events:
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE:
+                    self.change_to_scene( TitleScene() )
 
     def update(self):
         pass
@@ -336,9 +386,10 @@ class GameOver(Scene):
         TextUtil.display_message(surface, "Game Over")
 
 class Victory(Scene):
-    def __init__(self):
+    def __init__(self, hero):
         super().__init__()
-
+        self.hero = hero
+        
     def process_input(self, events, pressed_keys):
         for event in events:
             if event.type == pygame.KEYDOWN:
